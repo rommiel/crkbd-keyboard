1b49d78fa520be5bf9abd78c89bb32345478b8cb
rommiel <rommieljohnfrancisco@gmail.com>
2021-03-31 09:39:45 +0800

    crkbd woking layout with Luna working

---
+++ rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE     		= yes     		# Mouse keys
+RGBLIGHT_ENABLE     		= yes    		# Enable WS2812 RGB underlight
+EXTRAKEY_ENABLE				= yes			# enable windows media key
+OLED_DRIVER_ENABLE  		= yes
+WPM_ENABLE					= yes			# for pet luna
+LTO_ENABLE          		= yes
